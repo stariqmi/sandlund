4466ff63a0ce0ff0c0ea452283550ce97fa350e1
unknown <I840192@VANN00517798A.sap.corp>
2013-12-18 12:06:06 -0800

    pulling

--- pusher.js
+++ pusher.js
@@ -1,21 +1,22 @@
 var fs = require('fs');
 var Firebase = require('firebase');
 
-// var firebase = new Firebase('https://sandmansubmissions.firebaseIO.com/edgarSubmissions');
+var myDataRef = new Firebase('https://sandmansubmissions.firebaseio.com/submissions');
 
-// data = fs.readFileSync('json.txt')
-// var array = data.toString().split("\n");
-// for(var i in array) {
-// 	if(array[i].length != 0) {
-// 		es = JSON.parse(array[i]);
-// 		var pushed = firebase.push();
-// 		pushed.set(es)
-// 		console.log(pushed.toString());
-// 	}
-// }
-
-var dataRef = new Firebase("https://sandmansubmissions.firebaseio.com");
-dataRef.set("I am now writing data into Firebase!");
+data = fs.readFileSync('json.txt')
+var array = data.toString().split("\n");
+for(var i in array) {
+	if(array[i].length != 0) {
+		es = JSON.parse(array[i]);
+		var pushed = myDataRef.push(es, function(err) {
+			if(err) {
+				console.log(err);
+			}
+			else {
+				console.log("Data saved successfully!");
+			}
+		});
+	}
+}
 
 console.log("All stored in firebase!");
-process.exit();
---
+++ pusher_2.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var Firebase = require('firebase');
+var Deferred = require('JQDeferred');
+var promises = [];
+ 
+var firebase = new
+Firebase('http://sandmansubmissions.firebaseio.com/submissions/');
+ 
+data = fs.readFileSync('json.txt');
+var array = data.toString().split("\n");
+for(var i in array) {
+ if(array[i].length != 0) {
+   promises.push(Deferred(function(def) {
+      var row = JSON.parse(array[i]);
+      var pushed = firebase.push(row, function(err) {
+         console.log('finished', pushed.name());
+         if( err ) {
+            def.reject(err);
+         }
+         else {
+            def.resolve(err);
+         }
+      });
+      console.log('pushing', pushed.name());
+   }));
+ }
+}
+ 
+Deferred.when.apply(null, promises).done(function() {
+  console.log("All stored in firebase!");
+}).fail(function() {
+  Array.prototype.slice.call(arguments).forEach(function(err) {
+      console.error(err);
+  });
+}).always(process.exit.bind(process));
