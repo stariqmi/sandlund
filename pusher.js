efe5ae9d63e2b7679b560ffa6a0530f58ea701d6
salmantariqmirza <stariqmi@gmail.com>
2013-12-18 17:24:00 -0800

    pusher repaired

--- pusher.js
+++ pusher.js
@@ -2,28 +2,6 @@
 
 var fs = require('fs');
 var Firebase = require('firebase');
-<<<<<<< HEAD
-
-var myDataRef = new Firebase('https://sandmansubmissions.firebaseio.com/submissions');
-
-data = fs.readFileSync('json.txt')
-var array = data.toString().split("\n");
-for(var i in array) {
-	if(array[i].length != 0) {
-		es = JSON.parse(array[i]);
-		var pushed = myDataRef.push(es, function(err) {
-			if(err) {
-				console.log(err);
-			}
-			else {
-				console.log("Data saved successfully!");
-			}
-		});
-	}
-}
-
-console.log("All stored in firebase!");
-=======
 var Deferred = require('JQDeferred');
 var promises = [];
  
@@ -57,4 +35,3 @@ Deferred.when.apply(null, promises).done(function() {
       console.error(err);
   });
 }).always(process.exit.bind(process));
->>>>>>> 6cc8773c883d20f41094eca245793a3a59cf582b
